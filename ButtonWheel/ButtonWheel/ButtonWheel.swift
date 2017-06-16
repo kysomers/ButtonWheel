30fe2304da4da1f8b9539b9d488e14ecd5ec77ad
kysomers <kyledouglassomers@gmail.com>
2017-06-16 07:51:14 -0700

    Added test concepts to draw button pieces

--- ButtonWheel/ButtonWheel/ButtonWheel.swift
+++ ButtonWheel/ButtonWheel/ButtonWheel.swift
@@ -19,6 +19,7 @@ class ButtonWheel : UIView{
     var showNames = true
     var showPictures = true
     let backLayer =  UIView()
+    var hasDrawn = false
     
     //Any setter method for this will need to re-call setup
     public var middleRadius : CGFloat = 0
@@ -41,6 +42,7 @@ class ButtonWheel : UIView{
         backLayer.frame.setCenter(CGPoint(x: frame.width / 2, y: frame.height / 2))
         backLayer.layer.cornerRadius = dimensionSize / 2
         backLayer.clipsToBounds = true
+        self.addTestDrawPieceToView(myView: backLayer)
 
     }
     
@@ -77,6 +79,22 @@ class ButtonWheel : UIView{
         setupWith(names: names, pictureNames: nil, colors: colors, buttonNamesWillBeShown : buttonNamesWillBeShown)
     }
     
+    func addTestDrawPieceToView(myView : UIView){
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 200,y: 200), radius: CGFloat(80), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2 / 4), clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        //change the fill color
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        //you can change the stroke color
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        //you can change the line width
+        shapeLayer.lineWidth = 50.0
+        
+        myView.layer.addSublayer(shapeLayer)
+        print(shapeLayer.frame)
+    }
     
 
     
