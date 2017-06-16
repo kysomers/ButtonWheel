75f0849ac1a1ae122e33b8229c7b50c932a5a839
kysomers <kyledouglassomers@gmail.com>
2017-06-16 16:34:18 -0700

    Finished method to build the shape of a piece. Also modularized it into a static func in a helper struct

---
+++ ButtonWheel/ButtonWheel/ButtonViewBuilder.swift
@@ -0,0 +1,37 @@
+//
+//  ButtonViewBuilder.swift
+//  ButtonWheel
+//
+//  Created by Kyle Somers on 6/16/17.
+//  Copyright © 2017 Kyle Somers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ButtonViewBuilder {
+    static func createPiece(buttonWheel : ButtonWheel, color : UIColor, sectionNumber : Int) -> CAShapeLayer{
+        let startAngle = CGFloat(Double.pi / -2 +  Double.pi * 2 / Double(buttonWheel.numberOfSections) * Double(sectionNumber))
+        let endAngle = CGFloat(Double.pi / -2 +  Double.pi * 2 / Double(buttonWheel.numberOfSections) * Double(sectionNumber + 1))
+        let arcCenter = CGPoint(x: buttonWheel.backgroundView.frame.width / 2 ,y: buttonWheel.backgroundView.frame.height / 2)
+        
+        
+        let circlePath = UIBezierPath(arcCenter: arcCenter, radius: CGFloat((buttonWheel.dimensionSize / 2 + buttonWheel.middleRadius) / 2), startAngle: startAngle, endAngle:endAngle, clockwise: true)
+        
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        //change the fill color
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        //you can change the stroke color
+        shapeLayer.strokeColor = color.cgColor
+        //you can change the line width
+        shapeLayer.lineWidth = buttonWheel.dimensionSize / 2 - buttonWheel.middleRadius
+        
+        
+        return shapeLayer
+    }
+}
+
+
--- ButtonWheel/ButtonWheel/ButtonWheel.swift
+++ ButtonWheel/ButtonWheel/ButtonWheel.swift
@@ -14,11 +14,11 @@ class ButtonWheel : UIView{
     var buttons = [ButtonWheelPiece]()
     var borderColor  = UIColor.blue
     var middleColor = UIColor.green
-    var numberOfSections = 0
+    var numberOfSections = 5 //CHANGE THIS BACK TO 0
     var dimensionSize : CGFloat = 0
     var showNames = true
     var showPictures = true
-    let backLayer =  UIView()
+    let backgroundView =  UIView()
     var hasDrawn = false
     
     //Any setter method for this will need to re-call setup
@@ -29,21 +29,35 @@ class ButtonWheel : UIView{
     required init?(coder aDecoder: NSCoder) {
         
         super.init(coder: aDecoder)
+        configureBackgroundView()
+        
+
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureBackgroundView()
+    }
+    
+    func configureBackgroundView(){
         self.backgroundColor = .clear
         
         dimensionSize = self.frame.width < self.frame.height ? self.frame.width : self.frame.height
         
-        self.middleRadius = dimensionSize / 4
+        self.middleRadius = dimensionSize / 6
         
         
-        self.addSubview(backLayer)
-        backLayer.backgroundColor = middleColor
-        backLayer.frame.size = CGSize(width: dimensionSize, height: dimensionSize)
-        backLayer.frame.setCenter(CGPoint(x: frame.width / 2, y: frame.height / 2))
-        backLayer.layer.cornerRadius = dimensionSize / 2
-        backLayer.clipsToBounds = true
-        self.addTestDrawPieceToView(myView: backLayer)
+        self.addSubview(backgroundView)
+        backgroundView.backgroundColor = middleColor
+        backgroundView.frame.size = CGSize(width: dimensionSize, height: dimensionSize)
+        backgroundView.frame.setCenter(CGPoint(x: frame.width / 2, y: frame.height / 2))
+        backgroundView.layer.cornerRadius = dimensionSize / 2
+        backgroundView.clipsToBounds = true
 
+
+        
+        
+        
     }
     
     public func setupWith(names : [String], pictureNames : [String]?, colors : [UIColor], buttonNamesWillBeShown : Bool){
@@ -79,22 +93,7 @@ class ButtonWheel : UIView{
         setupWith(names: names, pictureNames: nil, colors: colors, buttonNamesWillBeShown : buttonNamesWillBeShown)
     }
     
-    func addTestDrawPieceToView(myView : UIView){
-        let circlePath = UIBezierPath(arcCenter: CGPoint(x: 200,y: 200), radius: CGFloat(80), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2 / 4), clockwise: true)
-        
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = circlePath.cgPath
-        
-        //change the fill color
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        //you can change the stroke color
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        //you can change the line width
-        shapeLayer.lineWidth = 50.0
-        
-        myView.layer.addSublayer(shapeLayer)
-        print(shapeLayer.frame)
-    }
+    
     
 
     
